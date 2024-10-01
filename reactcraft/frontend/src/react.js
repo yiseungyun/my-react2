3a4ff604c038c05869276608eb2eda777a55cdbd
yiseungyun <seungxxyun@gmail.com>
2024-10-01 20:46:03 +0900

    [feat] 가상 돔에 실제 돔 참조하는 ref 추가

--- reactcraft/frontend/src/react.js
+++ reactcraft/frontend/src/react.js
@@ -65,24 +65,39 @@ export const React = {
     container.appendChild(dom);
   },
 
-  _createVirtualDOM(element) {
+  _createVirtualDOM(element, prevVirtualDOM = null) {
     if (typeof element === 'string' || typeof element === 'number') {
-      return { type: 'TEXT_ELEMENT', props: { value: element } };
+      const virtualElement = { 
+        type: 'TEXT_ELEMENT', 
+        props: { value: element } 
+      };
+
+      if (prevVirtualDOM && prevVirtualDOM.type === 'TEXT_ELEMENT') {
+        virtualElement.ref = prevVirtualDOM.ref;
+      }
+
+      return virtualElement;
     }
 
     if (typeof element.type === 'function') {
       this._currentComponent = element.type;
       const componentElement = element.type(element.props);
-      return this._createVirtualDOM(componentElement);
+      return this._createVirtualDOM(componentElement, prevVirtualDOM);
     }
 
     const virtualElement = {
       type: element.type,
       props: {
         ...element.props,
-        children: (element.props.children || []).map(child => this._createVirtualDOM(child))
+        children: (element.props.children || []).map((child, index) => 
+          this._createVirtualDOM(child, prevVirtualDOM ? prevVirtualDOM.props.children[index] : null)
+        )
       }
     };
+
+    if (prevVirtualDOM) {
+      virtualElement.ref = prevVirtualDOM.ref;
+    }
 
     return virtualElement;
   },
@@ -90,6 +105,7 @@ export const React = {
   _createRealDOM(element) {
     if (element.type === 'TEXT_ELEMENT') {
       const textNode = document.createTextNode(element.props.value);
+      element.ref = textNode;
       return textNode;
     }
 
@@ -100,6 +116,7 @@ export const React = {
     }
 
     const dom = document.createElement(element.type);
+    element.ref = dom;
 
     Object.entries(element.props).forEach(([key, value]) => {
       if (key !== 'children') {
@@ -131,30 +148,30 @@ export const React = {
 
   _updatePatchDOM(parentDOM, prevDOM, currentDOM) {
     if (!prevDOM) {
-      parentDOM.appendChild(this._createRealDOM(currentDOM));
+      //parentDOM.appendChild(this._createRealDOM(currentDOM));
       return;
     }
 
     if (!currentDOM) {
-      parentDOM.removeChild(parentDOM.childNodes[0]);
+      //parentDOM.removeChild(parentDOM.childNodes[0]);
       return;
     }
 
     if (prevDOM.type !== currentDOM.type) {
-      parentDOM.replaceChild(this._createRealDOM(currentDOM), parentDOM.childNodes[0]);
+      //parentDOM.replaceChild(this._createRealDOM(currentDOM), parentDOM.childNodes[0]);
       return;
     }
 
     if (currentDOM.type === 'TEXT_ELEMENT') {
       if (prevDOM.props.value !== currentDOM.props.value) {
-        const textNode = parentDOM.childNodes[0];
-        textNode.nodeValue = currentDOM.props.value;
+        //const textNode = parentDOM.childNodes[0];
+        //textNode.nodeValue = currentDOM.props.value;
       }
       return;
     }
 
-    this._updateAttributes(parentDOM.childNodes[0], prevDOM.props, currentDOM.props);
-    this._updateChildren(parentDOM.childNodes[0], prevDOM.props.children, currentDOM.props.children);
+    //this._updateAttributes(, prevDOM.props, currentDOM.props);
+    //this._updateChildren(, prevDOM.props.children, currentDOM.props.children);
   },
 
   _updateAttributes(parentDOM, prevProps, currentProps) {
@@ -194,9 +211,11 @@ export const React = {
   _updateComponent(component) {
     this._currentComponent = component;
     this._stateIndex.set(component, 0);
-    const newVirtualDOM = this._createVirtualDOM(this._rootComponent);
+    const newVirtualDOM = this._createVirtualDOM(this._rootComponent, this._virtualDOM);
 
-    this._updatePatchDOM(this._container, this._virtualDOM, newVirtualDOM);
+    console.log(this._virtualDOM);
+    console.log(newVirtualDOM);
+    //this._updatePatchDOM(this._container, this._virtualDOM, newVirtualDOM);
 
     this._virtualDOM = newVirtualDOM;
   },
