3d11fccb9669ea344fb23f4c00a6e60b48bdbc1a
yiseungyun <seungxxyun@gmail.com>
2025-01-08 02:42:16 +0900

    [fix] 우선순위 스케줄링 추후 작업으로 미루고, 모두 동기 업데이트로 수정

--- frontend/src/react-reconciler/ReactFiberWorkLoop.js
+++ frontend/src/react-reconciler/ReactFiberWorkLoop.js
@@ -1,31 +1,13 @@
+import { beginWork } from "./ReactFiberBeginWork";
 import { commitRoot } from "./ReactFiberCommitWork";
+import { completeWork } from "./ReactFiberCompleteWork";
 
 function scheduleUpdateOnFiber(root) {
-  // 우선순위 계산
-  const priority = getCurrentPriorityLevel();
+  // TODO: 우선순위 계산
   
-  if (priority === SyncLane) { // 동기적으로 처리해야 하는 경우
-    performSyncWorkOnRoot(root);
-  } else { // 비동기 업데이트의 경우 스케줄링
-    ensureRootIsScheduled(root);
-  }
-}
-
-function ensureRootIsScheduled(root) { // 루트 스케줄링 보장
-  const existingCallbackNode = root.callbackNode;
-
-  // 이미 스케줄된 작업이 있다면 취소
-  if (existingCallbackNode !== null) {
-    cancelCallback(existingCallbackNode);
-  }
-
-  // 새로운 작업 스케줄링
-  const newCallbackNode = scheduleCallback(
-    schedulerPriorityLevel,
-    performConcurrentWorkOnRoot.bind(null, root)
-  );
-
-  root.callbackNode = newCallbackNode;
+  performSyncWorkOnRoot(root);
+  
+  // TODO: 비동기 업데이트의 경우 스케줄링 
 }
 
 function performSyncWorkOnRoot(root) {
@@ -61,10 +43,52 @@ function workLoopSync() {
   }
 }
 
+function performUnitOfWork(unitOfWork) {
+  // 현재 작업 중인 Fiber의 alternate 가져오기
+  const current = unitOfWork.alternate;
+
+  // beginWork를 호출하여 자식 Fiber들 처리
+  let next = beginWork(current, unitOfWork);
+
+  // pendingProps를 memoizedProps로 저장
+  // 이는 다음 렌더링에서 props 비교에 사용
+  unitOfWork.memoizedProps = unitOfWork.pendingProps;
+
+  if (next === null) {
+    // 자식이 없다면 완료 단계로 진입
+    completeUnitOfWork(unitOfWork);
+  } else {
+    // 자식이 있다면 다음 작업 단위를 설정
+    workInProgress = next;
+  }
+}
+
+function completeUnitOfWork(unitOfWork) {
+  let completedWork = unitOfWork;
+
+  do {
+    // 현재 Fiber의 alternate 가져오기
+    const current = completedWork.alternate;
+    // 부모 Fiber를 가져오기
+    const returnFiber = completedWork.return;
+
+    // 현재 Fiber 노드의 작업을 완료
+    completeWork(current, completedWork);
+
+    // 형제가 있다면 그 형제로 이동
+    const siblingFiber = completedWork.sibling;
+    if (siblingFiber !== null) {
+      // 형제가 있으면 workInProgress를 형제로 설정하고 종료
+      workInProgress = siblingFiber;
+      return;
+    }
+
+    // 형제가 없으면 부모로 올라가기
+    completedWork = returnFiber;
+    workInProgress = completedWork;
+  } while (completedWork !== null);
+}
+
 export {
-  scheduleUpdateOnFiber,
-  ensureRootIsScheduled,
-  performSyncWorkOnRoot,
-  renderRootSync,
-  workLoopSync
+  scheduleUpdateOnFiber
 }
