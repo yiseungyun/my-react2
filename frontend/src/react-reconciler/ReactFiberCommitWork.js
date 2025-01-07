813d7eedb0b21974a4ace324077cb1e0261fe285
yiseungyun <seungxxyun@gmail.com>
2025-01-08 02:48:12 +0900

    [WIP] commitRoot 함수 설계

--- frontend/src/react-reconciler/ReactFiberCommitWork.js
+++ frontend/src/react-reconciler/ReactFiberCommitWork.js
@@ -1,5 +1,24 @@
 function commitRoot(root) {
+  const finishedWork = root.finishedWork;
 
+  // 완료된 작업이 없으면 종료
+  if (finishedWork === null) {
+    return null;
+  }
+
+  root.finishedWork = null;
+
+  // 1. DOM 변경 전 필요한 정리 작업
+  commitBeforeMutationEffects(finishedWork);
+
+  // 2. DOM 변경
+  commitMutationEffects(root, finishedWork);
+
+  // 변경 완료된 트리를 current로 설정
+  root.current = finishedWork;
+
+  // 3. DOM 변경 후 작업 처리
+  commitLayoutEffects(finishedWork);
 }
 
 export {
