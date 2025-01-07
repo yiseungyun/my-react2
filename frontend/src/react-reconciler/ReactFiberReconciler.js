fdda33c3c9f599506dfb4cfab95c3e2b92f6cbe3
yiseungyun <seungxxyun@gmail.com>
2025-01-08 02:00:01 +0900

    [feat] render에서 호출하는 updateContainer 구현

--- frontend/src/react-reconciler/ReactFiberReconciler.js
+++ frontend/src/react-reconciler/ReactFiberReconciler.js
@@ -1,5 +1,20 @@
-function updateContainer() {
+import { scheduleUpdateOnFiber } from "./ReactFiberWorkLoop";
+import { enqueueUpdate } from "./ReactUpdateQueue";
 
+function updateContainer(element, root) {
+  const current = root.current;
+  
+  // 업데이트 생성 및 예약
+  const update = {
+    payload: { element },
+    next: null
+  };
+  
+  // 업데이트 큐에 추가
+  enqueueUpdate(current, update);
+  
+  // 렌더링 스케줄링 시작
+  scheduleUpdateOnFiber(root);
 }
 
 export {
