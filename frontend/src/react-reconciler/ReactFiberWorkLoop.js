393df15d308f33ae90b33f1340d44d41614a73c5
yiseungyun <seungxxyun@gmail.com>
2025-01-08 02:03:02 +0900

    [feat] 업데이트 스케줄링 함수 구현

--- frontend/src/react-reconciler/ReactFiberWorkLoop.js
+++ frontend/src/react-reconciler/ReactFiberWorkLoop.js
@@ -1,7 +1,13 @@
-function scheduleUpdateOnFiber() {
-
+function scheduleUpdateOnFiber(root) {
+  // 1. 우선순위 계산
+  const priority = getCurrentPriorityLevel();
+  
+  if (priority === SyncLane) { // 2. 동기적으로 처리해야 하는 경우
+    performSyncWorkOnRoot(root);
+  } else { // 3. 비동기 업데이트의 경우 스케줄링
+    ensureRootIsScheduled(root);
+  }
 }
-
 export {
   scheduleUpdateOnFiber
 }
